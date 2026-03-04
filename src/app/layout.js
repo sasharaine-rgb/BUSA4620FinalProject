a7fba9f75ed24c1d5bb2c49941b5e08155a254eb
sasharaine-rgb <sasha_raine@icloud.com>
2026-03-03 19:52:06 -0800

    google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,9 +7,24 @@ export const metadata = {
   description: "Find hikes in Banff by difficulty.",
 };
 
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-CX05JDB5G4"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-CX05JDB5G4');
+            `,
+          }}
+        />
+      </head>
       <body>
         <Navigation />
         {children}
